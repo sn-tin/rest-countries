95e76e32432b6344cb51514d528948d9536487ef
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-11-13 20:08:25 +0800

    Restart project and install styled-components - Remove code in main and countries card files - Uninstall sass and install styled-components for the UI codes

--- src/components/CountriesCard.js
+++ src/components/CountriesCard.js
@@ -1,22 +1,7 @@
 
-const CountriesCard = (data) => {
-    const { flag, countryName, population, region, capital } = data; 
+const CountriesCard = () => {
     return (
-        <div className="country-card">
-            <img src={flag} alt={countryName} />
-            <h2>{countryName}</h2>
-            <ul className="country-details">
-                <li className="population">
-                    <p><span>Population: </span>{population}</p>
-                </li>
-                <li className="region">
-                    <p><span>Region: </span>{region}</p>
-                </li>
-                <li className="capital">
-                    <p><span>Capital: </span>{capital}</p>
-                </li>
-            </ul>
-        </div>
+        <h1>Countries</h1>       
     )
 }
 
