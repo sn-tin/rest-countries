03c5b0567e2f49ccde2f913ef216febf7a90991c
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-12-01 22:51:05 +0800

    Add styles for pseudo-class focus and add input color - Remove outline when input field is on focus - Add text color for input field

--- src/components/Main/Main.styles.js
+++ src/components/Main/Main.styles.js
@@ -6,6 +6,7 @@ const StyledMain = styled.main`
 `
 const Searchbar = styled.div`
     width: 100%;
+    max-width: 25rem;
     border-radius: 5px;
     padding: 12px 30px;
     background-color: ${({theme}) => theme.secondary};
@@ -13,12 +14,16 @@ const Searchbar = styled.div`
         color: ${({theme}) => theme.text};
     }
     input {
+        color: ${({theme}) => theme.text};
         border: none;
         background-color: ${({theme}) => theme.secondary};
         margin-left: 30px;
         &::placeholder {
             color: ${({theme}) => theme.text};
             font-size: clamp(0.9rem, 2.5vw, 1rem);
+        }
+        &:focus {
+            outline: none;
         }
     }
 `
