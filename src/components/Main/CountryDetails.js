f24944ae4fca664d6c6f029eb5adb77458dfe9a3
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-12-10 22:35:22 +0800

    Add content to CountryDetails depending on what country is clicked in cards of countries

--- src/components/Main/CountryDetails.js
+++ src/components/Main/CountryDetails.js
@@ -1,16 +1,18 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const CountryDetails = ({countries}) => {
     const [country, setCountry] = useState(null);
-    const { flags, population, region, subregion, capital, tld, languages } = country;
+    // const { flags, population, region, subregion, capital, tld, languages } = country;
     let native = country.name.nativeName[Object.keys(country.name.nativeName)[0]].common
     let currency = country.currencies[Object.keys(country.currencies)[0]].name
+    // let language = country.languages[Object.keys(country.languages)[0]]
     const { slug } = useParams();
     const name = slug.split("-").join(" ");
 
     let foundCountry = countries.find(data => data.name.common === name);
+    console.log(country)
     useEffect(() => {
         setCountry(foundCountry)
         if(!foundCountry) {
@@ -23,22 +25,26 @@ const CountryDetails = ({countries}) => {
 
     return (
         <div className="country-details">
-            <button>Back</button>
+            <Link to='/'>
+                <button>Back</button>
+            </Link>
             <div className="details">
-                {/* <img src={flags.svg} alt={`${name}'s flag`} /> */}
+                <img src={country.flags.svg} alt={`${name}'s flag`} />
                 <div className="content-wrapper">
                     <h1>{name}</h1>
                     <div className="content-1">
                         <p>Native Name: <span>{native}</span></p>
-                        {/* <p>Population: <span>{formatPopulation(country.population)}</span></p> */}
-                        <p>Region: <span>{region}</span></p>
-                        <p>Sub-Region: <span>{subregion}</span></p>
-                        <p>Capital: <span>{capital}</span></p>
+                        <p>Population: <span>{formatPopulation(country.population)}</span></p>
+                        <p>Region: <span>{country.region}</span></p>
+                        <p>Sub-Region: <span>{country.subregion}</span></p>
+                        <p>Capital:
+                            {country.capital?.map(cap => <span> {cap}</span>)}
+                        </p>
                     </div>
                     <div className="content-2">
-                        <p>Top Level Domain: <span>{tld[0]}</span></p>
+                        <p>Top Level Domain: <span>{country.tld[0]}</span></p>
                         <p>Currencies: <span>{currency}</span></p>
-                        {/* <p>Languages: <span>{languages}</span></p> */}
+                        {/* <p>Languages: <span>{language}</span></p> */}
                     </div>
                 </div>
             </div>
