6dee30cb758cba2bd2534ee573fa4d48c42338f4
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-12-12 19:07:40 +0800

    Fix code to render data - Check if the country state is already populated before rendering the details by adding country && (<div></div>) - Add ternary operation in native, currency and language variables - Map borders and return buttons

--- src/components/Main/CountryDetails.js
+++ src/components/Main/CountryDetails.js
@@ -2,56 +2,71 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-const CountryDetails = ({countries}) => {
-    const [country, setCountry] = useState([]);
-    let native = country.name.nativeName[Object.keys(country.name.nativeName)[0]].common;
-    let currency = country.currencies[Object.keys(country.currencies)[0]].name
-    let language = country.languages[Object.keys(country.languages)[0]];
+const CountryDetails = ({ countries }) => {
+    const [country, setCountry] = useState(null);
+    let native = country
+      ? country.name.nativeName[Object.keys(country.name.nativeName)[0]].common
+      : "";
+    let currency = country
+      ? country.currencies[Object.keys(country.currencies)[0]].name
+      : "";
+    let language = country
+      ? country.languages[Object.keys(country.languages)[0]] 
+      : "";
+
     const { slug } = useParams();
     const name = slug.split("-").join(" ");
-
-    let foundCountry = countries.find(data => data.name.common === name);
-    console.log(country)
+  
+    let foundCountry = countries.find((data) => data.name.common === name);
     useEffect(() => {
-        setCountry(foundCountry)
-        if(!foundCountry) {
-            return <h1>Loading...</h1>
-        }
-    }, [foundCountry])
-
-    /* Format mumbers with commas */ 
-    const formatPopulation = (value) => value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
+      setCountry(foundCountry);
+      console.log(foundCountry);
+    }, [foundCountry]);
+  
+    /* Format mumbers with commas */
+  
+    const formatPopulation = (value) =>
+      value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  
     return (
-        <div className="country-details">
-            <Link to='/'>
-                <button>
-                    <i class="fa-solid fa-arrow-left"></i>
-                    Back
-                </button>
-            </Link>
-            <div className="details">
-                <img src={country.flags.svg} alt={`${name}'s flag`} />
-                <div className="content-wrapper">
-                    <h1>{name}</h1>
-                    <div className="content-1">
-                        <p>Native Name: <span>{native}</span></p>
-                        <p>Population: <span>{formatPopulation(country.population)}</span></p>
-                        <p>Region: <span>{country.region}</span></p>
-                        <p>Sub-Region: <span>{country.subregion}</span></p>
-                        <p>Capital:
-                            {country.capital?.map(cap => <span> {cap}</span>)}
-                        </p>
-                    </div>
-                    <div className="content-2">
-                        <p>Top Level Domain: <span>{country.tld[0]}</span></p>
-                        <p>Currencies: <span>{currency}</span></p>
-                        <p>Languages: <span>{language}</span></p>
-                    </div>
-                </div>
+      <div className="country-details">
+        <Link to="/">
+          <button>
+            <i className="fa-solid fa-arrow-left"></i>
+            Back
+          </button>
+        </Link>
+        {country && (
+          <div className="details">
+            <img src={country?.flags.svg} alt={`${name}'s flag`} />
+            <div className="content-wrapper">
+              <h1>{name}</h1>
+              <div className="content-1">
+                <p>Native Name: <span>{native}</span></p>
+                <p>Population: <span>{formatPopulation(country.population)}</span></p>
+                <p>Region: <span>{country.region}</span></p>
+                <p>Sub-Region: <span>{country.subregion}</span></p>
+                <p>Capital:
+                  {country.capital?.map((cap) => (
+                    <span key={cap[0]}> {cap}</span>
+                  ))}
+                </p>
+              </div>
+              <div className="content-2">
+                <p>Top Level Domain: <span>{country.tld[0]}</span></p>
+                <p>Currencies: <span>{currency}</span></p>
+                <p>Language: <span>{language}</span></p>
+              </div>
             </div>
-        </div>
-    )
-}
-
-export default CountryDetails;
+            <div className="border-countries">
+                {country.borders.map(border => (
+                    <button key={border}>{border}</button>
+                ))}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default CountryDetails;  
