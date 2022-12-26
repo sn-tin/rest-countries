a7a3f64946c450617d194d333c2aafde5766f276
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-12-26 22:37:17 +0800

    Switch moon icons of light and dark theme

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({theme, changeTheme}) => {
             <h1>Where in the world?</h1>
             <ModeButton onClick={changeTheme}>
             {
-                theme === "dark" ? <i className="fa-regular fa-moon"></i> : <i className="fa-solid fa-moon"></i>
+                theme === "dark" ? <i className="fa-solid fa-moon"></i> : <i className="fa-regular fa-moon"></i>
             }
             { theme === "dark" ? "Light Mode" : "Dark Mode" }
             </ModeButton>
